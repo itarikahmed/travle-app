3f759bd33448668c76fee8327ceacbbef1a858fb
itarikahmed <tarik.ahmed@northsouth.edu>
2024-11-03 17:28:48 +0600

    fix google font issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,17 @@
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata = {
   title: "Travel App",
   description: "This is a travel app",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   return (
