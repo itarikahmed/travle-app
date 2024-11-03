d568dc5519e8cff4a2310c2d6f98052901026ad9
itarikahmed <tarik.ahmed@northsouth.edu>
2024-11-03 17:06:22 +0600

    update readme

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} m-0 p-0 box-border antialiased relative overflow-hidden`}
+        className={`${inter.className} m-0 p-0 box-border relative overflow-hidden`}
       >
         <Navbar />
         {children}
--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,12 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Hero />
       <Camp />
       <Guide />
       <Features />
       <GetApp />
-    </>
+    </div>
   );
 }
